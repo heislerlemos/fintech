5ad5bee6d9431a777c6f2296b844b97d54ee9b6f
Heisler Lemos <heisler.lemos@gmail.com>
2025-11-27 17:05:07 +0100

    working on post  ...

--- src/main/java/ao/kwanzazap/fintech/Conta.java
+++ src/main/java/ao/kwanzazap/fintech/Conta.java
@@ -1,22 +1,33 @@
 package ao.kwanzazap.fintech;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
+
 
 
 
 public class Conta {
+    @Id
     private Integer numero_de_conta;
     private Integer saldo;
     private String nome_do_titular;
     private String iban;
     private Integer pin;
+    @Enumerated(EnumType.STRING)
     private Movimentos movimentos;
+    @Enumerated(EnumType.STRING)
     private Servicos servicos;
 
 }
--- src/main/java/ao/kwanzazap/fintech/ContaController.java
+++ src/main/java/ao/kwanzazap/fintech/ContaController.java
@@ -2,20 +2,39 @@ package ao.kwanzazap.fintech;
 
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import ao.kwanzazap.fintech.Servicos;
+import ao.kwanzazap.fintech.Movimentos;
+import ao.kwanzazap.fintech.ContaRepository;
+import ao.kwanzazap.fintech.Conta;
 
  @Controller
 
 public class ContaController
 {
+    @Autowired
+    private ContaRepository contaRepository;
 
     @ResponseBody
     @RequestMapping("/contas")
     public List<Conta> contar (){
         Conta conta = new Conta (192929, 2500,"Moises Baltazar", "AO0060002223339922",330022, Movimentos.DEPOSITO, Servicos.UNITEL );
         return Arrays.asList(conta, conta, conta, conta, conta);
+    }
+
+
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/contas", method = RequestMethod.POST)
+    public void guardar (@RequestBody Conta conta) {
+        contaRepository.save(conta);
+
     }
 }
