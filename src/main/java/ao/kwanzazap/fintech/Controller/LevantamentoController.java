1d630087ad6623a21893ed9fd8f169cf296b2fda
Heisler Lemos <heisler.lemos@gmail.com>
2025-12-10 15:20:55 +0100

    added transfer and withdraw movement

--- src/main/java/ao/kwanzazap/fintech/Controller/LevantamentoController.java
+++ src/main/java/ao/kwanzazap/fintech/Controller/LevantamentoController.java
@@ -1,9 +1,19 @@
 package ao.kwanzazap.fintech.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 
 public class LevantamentoController {
+
+    @RequestMapping("/levantamento/{id}")
+
+    public String  verlevantamento(){
+        return  "levantamento";
+
+    }
+
 }
