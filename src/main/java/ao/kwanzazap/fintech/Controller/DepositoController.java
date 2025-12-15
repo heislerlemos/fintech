f0b1e9da045b1fc97074f4f2a6ba20ed15d5f996
Heisler Lemos <heisler.lemos@gmail.com>
2025-12-15 16:16:11 +0100

    working on displaying balance by account on deposit and withdraw

--- src/main/java/ao/kwanzazap/fintech/Controller/DepositoController.java
+++ src/main/java/ao/kwanzazap/fintech/Controller/DepositoController.java
@@ -1,13 +1,36 @@
 package ao.kwanzazap.fintech.Controller;
 
+import ao.kwanzazap.fintech.Interface.ContaRepository;
 import ao.kwanzazap.fintech.Model.Conta;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DepositoController {
+    @Autowired
+    private ContaRepository contaRepository;
+/*
+    @RequestMapping("/deposito/{id}")
+
+    public String  verdeposito(Model model , @RequestParam long id){
+        Conta conta = contaRepository.findById(id).orElse(null);
+
+        Conta conta1 = new Conta();
+        conta1.setBalanco(conta.getBalanco());
+
+        model.addAttribute("conta", conta);
+        model.addAttribute("conta1", conta1);
+
+        return  "deposito";
+
+    }
+
+*/
+
 
     @RequestMapping("/deposito/{id}")
 
@@ -15,7 +38,4 @@ public class DepositoController {
         return  "deposito";
 
     }
-
-
-
 }
