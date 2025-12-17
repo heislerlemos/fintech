05510ac9aed84ddca1d558e02d5c39ba75cb9963
Heisler Lemos <heisler.lemos@gmail.com>
2025-12-17 13:53:42 +0100

    added withdraw and hidded the id

--- src/main/java/ao/kwanzazap/fintech/Controller/DepositoController.java
+++ src/main/java/ao/kwanzazap/fintech/Controller/DepositoController.java
@@ -5,11 +5,9 @@ import ao.kwanzazap.fintech.Model.Conta;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class DepositoController {
--- src/main/java/ao/kwanzazap/fintech/Controller/LevantamentoController.java
+++ src/main/java/ao/kwanzazap/fintech/Controller/LevantamentoController.java
@@ -1,19 +1,43 @@
 package ao.kwanzazap.fintech.Controller;
 
+import ao.kwanzazap.fintech.Interface.ContaRepository;
+import ao.kwanzazap.fintech.Model.Conta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 
 public class LevantamentoController {
 
-    @RequestMapping("/levantamento/{id}")
+    @Autowired
+    private ContaRepository contaRepository;
 
-    public String  verlevantamento(){
+
+    @GetMapping("/levantamento/{id}")
+
+    public String  verlevantamento(Model model , @PathVariable long id){
+
+        try {
+            Conta conta = contaRepository.findById(id).get();
+            model.addAttribute("conta", conta);
+
+            Conta conta1 = new Conta();
+            conta1.setId(conta.getId());
+            conta1.setBalanco(conta.getBalanco());
+
+            model.addAttribute("conta1", conta1);
+
+        } catch (Exception e){
+            System.out.println("Erro:"+ e.getMessage());
+            return "redirect:index";
+        }
         return  "levantamento";
 
     }
+
 
 }
